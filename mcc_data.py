9c0dfe268058f4f746882b9fe6d72bd2550771dc
2bitfox <94010300+2bitfox@users.noreply.github.com>
2022-06-23 17:48:07 -0500

    pulls all data from all the teams

--- mcc_data.py
+++ mcc_data.py
@@ -9,35 +9,203 @@ mc = MCC()
 rd = mc.rundown()
 player_points = rd.json()["data"]["individualScores"]
 
-# get yellow team from the api
+# pull team data
+
+red_team = mc.teams("Red").json()
+red_names = []
+
+orange_team = mc.teams("Orange").json()
+orange_names = []
+
 yellow_team = mc.teams("Yellow").json()
 yellow_names = []
 
-# grab the usernames of everyone on yellow team
+lime_team = mc.teams("Lime").json()
+lime_names = []
+
+green_team = mc.teams("Green").json()
+green_names = []
+
+aqua_team = mc.teams("Aqua").json()
+aqua_names = []
+
+cyan_team = mc.teams("Cyan").json()
+cyan_names = []
+
+blue_team = mc.teams("Blue").json()
+blue_names = []
+
+purple_team = mc.teams("Purple").json()
+purple_names = []
+
+pink_team = mc.teams("Pink").json()
+pink_names = []
+
+
+# grab the usernames of everyone
+
 x = 0
 while x < 4:
+    name = red_team["data"][x]["username"]
+    red_names.append(name)
+
+    name = orange_team["data"][x]["username"]
+    orange_names.append(name)
+
     name = yellow_team["data"][x]["username"]
     yellow_names.append(name)
+
+    name = lime_team["data"][x]["username"]
+    lime_names.append(name)
+
+    name = green_team["data"][x]["username"]
+    green_names.append(name)
+
+    name = aqua_team["data"][x]["username"]
+    aqua_names.append(name)
+
+    name = cyan_team["data"][x]["username"]
+    cyan_names.append(name)
+
+    name = blue_team["data"][x]["username"]
+    blue_names.append(name)
+
+    name = purple_team["data"][x]["username"]
+    purple_names.append(name)
+
+    name = pink_team["data"][x]["username"]
+    pink_names.append(name)
+
     x += 1
 
-print (yellow_names)
+# grab scores
 
-# grab their points
-scores = []
+rscores = []
+for i in red_names:
+    rscores.append(player_points[i])
+
+oscores = []
+for i in orange_names:
+    oscores.append(player_points[i])
+
+yscores = []
 for i in yellow_names:
-    scores.append(player_points[i])
+    yscores.append(player_points[i])
 
-print(scores)
+lscores = []
+for i in lime_names:
+    lscores.append(player_points[i])
 
-# example plot. probably a better way to display this but this is just to show an example
-yellow = pd.DataFrame(
+gscores = []
+for i in green_names:
+    gscores.append(player_points[i])
+
+ascores = []
+for i in aqua_names:
+    ascores.append(player_points[i])
+
+cscores = []
+for i in cyan_names:
+    cscores.append(player_points[i])
+
+bscores = []
+for i in blue_names:
+    bscores.append(player_points[i])
+
+pscores = []
+for i in purple_names:
+    pscores.append(player_points[i])
+
+piscores = []
+for i in pink_names:
+    piscores.append(player_points[i])
+
+# turn the data into a dataframe
+teams = pd.DataFrame(
     {
-        "Username":yellow_names,
-        "Coins": scores
+        "Username": red_names,
+        "Coins": rscores,
+        "Color": "Red"
     }
 )
 
-print(yellow)
+oteam = pd.DataFrame(
+    {
+        "Username": orange_names,
+        "Coins": oscores,
+        "Color": "Orange"
+    }
+)
 
-plot = px.histogram(yellow, x="Username", y="Coins")
+yteam = pd.DataFrame(
+    {
+        "Username": yellow_names,
+        "Coins": yscores,
+        "Color": "Yellow"
+    }
+)
+
+
+lteam = pd.DataFrame(
+    {
+        "Username": lime_names,
+        "Coins": lscores,
+        "Color": "Lime"
+    }
+)
+
+gteam = pd.DataFrame(
+    {
+        "Username": green_names,
+        "Coins": gscores,
+        "Color": "Green"
+    }
+)
+
+ateam = pd.DataFrame(
+    {
+        "Username": aqua_names,
+        "Coins": ascores,
+        "Color": "Aqua"
+    }
+)
+
+cteam = pd.DataFrame(
+    {
+        "Username": cyan_names,
+        "Coins": cscores,
+        "Color": "Cyan"
+    }
+)
+
+bteam = pd.DataFrame(
+    {
+        "Username":blue_names,
+        "Coins": bscores,
+        "Color": "Blue"
+    }
+)
+
+pteam = pd.DataFrame(
+    {
+        "Username": purple_names,
+        "Coins": pscores,
+        "Color": "Purple"
+    }
+)
+
+piteam = pd.DataFrame(
+    {
+        "Username": pink_names,
+        "Coins": piscores,
+        "Color": "Pink"
+    }
+)
+
+teams = teams.append([oteam, yteam, lteam, gteam, ateam, cteam, bteam, pteam, piteam])
+
+teams.to_csv("out.csv")
+print(teams)
+
+plot = px.histogram(teams, x="Color", y="Coins", color="Username")
 plot.show()
